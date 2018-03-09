bd88b3d601acde6cb59a68da1dab4cf67f1f885b
NoSocksForYou <kaappo.raivio@gmail.com>
2018-03-09 12:02:11 +0200

    init

---
+++ summa-algoritmi.py
@@ -0,0 +1,29 @@
+def kolmioluku(luku, aloituspiste=1):
+    mahdolliset = []
+    while aloituspiste < luku:
+        temp_summa = []
+        temp = aloituspiste
+        while sum(temp_summa) < luku:
+            temp_summa.append(temp)
+            temp += 1
+            if sum(temp_summa) == luku:
+                mahdolliset.append(temp_summa)
+        aloituspiste += 1
+    return mahdolliset
+
+
+def prettyPrint(mahdolliset):
+    master_temp = []
+    for i in mahdolliset:
+        temp = ''
+        for a in i:
+            temp += str(a)
+            temp += ' + '
+        temp = temp[:len(temp) - 3]
+        master_temp.append(temp)
+    return '\n\t'.join(master_temp)
+    # return ','.join(master_temp)
+
+
+for i in range(100):
+    print('{}:\t{}\n'.format(i, prettyPrint(kolmioluku(i))))   # prettyprint tapa
