c0c7fe1b013ca92632df0605c9dc1acf4bf8e8f2
NoSocksForYou <kaappo.raivio@gmail.com>
2018-03-13 10:26:10 +0200

    asd

--- summa-algoritmi.py
+++ summa-algoritmi.py
@@ -1,15 +1,24 @@
+
+def tulo(lista):
+    temp = 1
+    for i in lista:
+        temp *= i
+    return temp
+
+
 def kolmioluku(luku, aloituspiste=1):
     mahdolliset = []
     while aloituspiste < luku:
         temp_summa = []
         temp = aloituspiste
-        while sum(temp_summa) < luku:
+        while tulo(temp_summa) < luku:
             temp_summa.append(temp)
             temp += 1
-            if sum(temp_summa) == luku:
+            if tulo(temp_summa) == luku:
                 mahdolliset.append(temp_summa)
         aloituspiste += 1
     return mahdolliset
+
 
 
 def prettyPrint(mahdolliset):
@@ -18,7 +27,7 @@ def prettyPrint(mahdolliset):
         temp = ''
         for a in i:
             temp += str(a)
-            temp += ' + '
+            temp += ' * '
         temp = temp[:len(temp) - 3]
         master_temp.append(temp)
     return '\n\t'.join(master_temp)
